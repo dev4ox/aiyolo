d781e4b908984ecb88cb5a9ce009ddc724e2452e
Ten-o69 <159043573+Ten-o69@users.noreply.github.com>
2024-03-12 12:15:04 +0300

    Update api.py

--- api.py
+++ api.py
@@ -15,9 +15,9 @@ from config import (
     DIR_IMAGES,
     FILE_TYPE_GET_IMAGE,
     DIR_RUNS,
+    CLASSES,
 )
 from model import Model
-
 
 app = FastAPI(
     title=API_TITLE,
@@ -29,7 +29,7 @@ app = FastAPI(
 model = Model()
 
 
-def _create_image_file(img: Annotated[UploadFile, File()]) -> str:
+async def _create_image_file(img: Annotated[UploadFile, File()]) -> str:
     # create dir to "get images"
     if not os.path.exists(DIR_IMAGES):
         os.mkdir(DIR_IMAGES)
@@ -46,19 +46,9 @@ def _create_image_file(img: Annotated[UploadFile, File()]) -> str:
     return image_path
 
 
-@app.patch("/api/patch_image",
-           name="patch_image",
-           description="drg",
-           responses={
-               200: {
-                   "content": {"image/png": {}}
-               }
-           },
-           # response_class=Response
-           )
-async def get_image(img: Annotated[UploadFile, File()]):  # get image to predict
+async def _get_predict_path(image: Annotated[UploadFile, File()]) -> str:
     # create image file from "get image"
-    image_path = _create_image_file(img)
+    image_path = await _create_image_file(image)
 
     # predict "get image"
     predict_done = model.predict(image_path)
@@ -76,13 +66,71 @@ async def get_image(img: Annotated[UploadFile, File()]):  # get image to predict
                     predict_paths.append(directory)
 
         now_predict_num = max(dir_nums)
-        with open(DIR_RUNS + f"detect/predict{now_predict_num}/" + Path(image_path).name, "rb") as image:
-            image_bytes_return = image.read()
-
-        return Response(content=image_bytes_return, media_type="image/png")
+        return DIR_RUNS + f"detect/predict{now_predict_num}/" + Path(image_path).name
 
     else:
         raise
+
+
+@app.put(
+    "/api/patch_image",
+    name="patch_image",
+    description="На вход принимает изображение для обработки. "
+                "Вернёт обработано изображение с выделными классами, которые были распознаны.",
+    responses={
+        200: {
+            "content": {"image/png": {}}
+        }
+    },
+    response_class=Response
+)
+async def get_image(image: Annotated[UploadFile, File()]) -> Response:  # get image to predict
+    # get now predict path
+    now_predict_path = await _get_predict_path(image)
+
+    # open predict image and get bytes of image
+    with open(now_predict_path, "rb") as image_file:
+        image_bytes_return = image_file.read()
+
+    return Response(content=image_bytes_return, media_type="image/png")
+
+
+@app.put(
+    "/api/patch_image_txt",
+    name="patch_image_txt",
+    description="На вход принимает изображение для обработки. "
+                "Вернёт список распознаных классов в текстовом варианте.",
+    responses={
+        200: {
+            "content": {"image/txt": {}}
+        }
+    },
+)
+async def get_image_txt(image: Annotated[UploadFile, File()]) -> str:
+    now_predict_path = Path(await _get_predict_path(image))
+    path_parent = str(now_predict_path.parent)
+    path_name = now_predict_path.name
+    path_suffix = now_predict_path.suffix
+
+    with open(path_parent + f"/labels/{path_name[:-len(path_suffix)]}.txt", "r") as predict_file_txt:
+        data = list(predict_file_txt.read())
+        text_temp = ""
+        count_string = 0
+        strings_dict = []
+
+        for value in data:
+            text_temp += value
+
+            if value == "\n":
+                strings_dict.append(text_temp)
+                text_temp = ""
+                count_string += 1
+
+        text_temp = ""
+        for string in strings_dict:
+            text_temp += CLASSES[int(list(string)[0] + list(string)[1])]
+
+    return text_temp
 
 
 def start_local_server(server_port: int | None = None) -> None:  # start local server to api
